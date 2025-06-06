42f304e983b64319ad2fbae6f8b043e46e92029b
justine111 <90599321+justine111@users.noreply.github.com>
2025-06-07 07:21:07 +0800

    .

--- src/pages/main/main.php
+++ src/pages/main/main.php
@@ -48,7 +48,7 @@
             </li>
           </ol>
         </nav>
-        <h2 class="mt-3 text-lg font-semibold text-orange-500 dark:text-white">  
+        <h2 class="mt-3 text-lg font-semibold text-orange-500 dark:text-white">
           Popular Space Spot for Your Needs.
         </h2>
       </div>
@@ -96,14 +96,14 @@
       </div>
     </div>
     <div class="mb-4 grid gap-4 sm:grid-cols-2 md:mb-8 lg:grid-cols-4 xl:grid-cols-4">
-      <div class="rounded-lg border border-gray-200 bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800">
+      <div class="rounded-lg border border-gray-200 bg-white shadow-lg dark:border-gray-700 dark:bg-gray-800">
         <div class="h-56 w-full">
           <a href="#">
-            <img class="mx-auto h-full rounded-lg" src="<?= $basePath ?>/src/repositories/uploads/pic1.jpg" alt="" />
+            <img class="mx-auto h-full rounded-t-lg" src="<?= $basePath ?>/src/repositories/uploads/pic1.jpg" alt="" />
             <img class="mx-auto hidden h-full dark:block" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-front-dark.svg" alt="" />
           </a>
         </div>
-        <div class="pt-6">
+        <div class="pt-2 p-4">
           <div class="mb-4 flex items-center justify-between gap-4">
             <span class="me-2 rounded bg-primary-100 px-2.5 py-0.5 text-xs font-medium text-primary-800 dark:bg-primary-900 dark:text-primary-300"> Up to 35% off </span>
 
@@ -190,15 +190,15 @@
           </div>
         </div>
       </div>
-      <div class="rounded-lg border border-gray-200 bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800">
+      <div class="rounded-lg border border-gray-200 bg-white shadow-lg dark:border-gray-700 dark:bg-gray-800">
         <div class="h-56 w-full">
           <a href="#">
-            <img class="mx-auto h-full rounded-lg" src="<?= $basePath ?>/src/repositories/uploads/pic2.jpg" alt="" />
+            <img class="mx-auto h-full rounded-t-lg" src="<?= $basePath ?>/src/repositories/uploads/pic2.jpg" alt="" />
             <img class="mx-auto hidden h-full dark:block" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/iphone-dark.svg" alt="" />
           </a>
         </div>
 
-        <div class="pt-6">
+        <div class="pt-2 p-4">
           <div class="mb-4 flex items-center justify-between gap-4">
             <span class="me-2 rounded bg-primary-100 px-2.5 py-0.5 text-xs font-medium text-primary-800 dark:bg-primary-900 dark:text-primary-300"> Up to 15% off </span>
 
@@ -385,7 +385,7 @@
           </div>
         </div>
       </div>
-      <div class="rounded-lg border border-gray-200 bg-white p-6 shadow-sm dark:border-gray-700 dark:bg-gray-800">
+      <div class="rounded-lg border border-gray-200 bg-white shadow-sm dark:border-gray-700 dark:bg-gray-800">
         <div class="h-56 w-full">
           <a href="#">
             <img class="mx-auto h-full dark:hidden" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/ps5-light.svg" alt="" />
@@ -393,7 +393,7 @@
           </a>
         </div>
 
-        <div class="pt-6">
+        <div class="pt-2 p-4">
           <div class="mb-4 flex items-center justify-between gap-4">
             <span class="me-2 rounded bg-primary-100 px-2.5 py-0.5 text-xs font-medium text-primary-800 dark:bg-primary-900 dark:text-primary-300"> Up to 10% off </span>
 
