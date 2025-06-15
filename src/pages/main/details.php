d757b08512f65d3a1c0c08d6d67152d7cc7889eb
Mark Justine Bengzon <90599321+justine111@users.noreply.github.com>
2025-06-15 21:04:41 +0800

    .

--- src/pages/main/details.php
+++ src/pages/main/details.php
@@ -7,76 +7,30 @@ $roomId = $_GET['id'];
 $roomDetails = $roomController->getHotelById($roomId);
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>StaySmart</title>
-  <link rel="icon" type="image/png" href="/booking-sys/src/assets/img/logo.gif">
-  <link href="/booking-sys/src/assets/css/output.css" rel="stylesheet">
-  <link href="/booking-sys/src/assets/css/vendor/flowbite.min.css" rel="stylesheet">
-  <link href="/booking-sys/src/assets/css/vendor/fontawesome/css/all.css" rel="stylesheet" />
-  <link href="/booking-sys/src/assets/css/vendor/sweet-alert2.css" rel="stylesheet" />
-  <link href="/booking-sys/src/assets/css/vendor/toastr/toastr.min.css" rel="stylesheet" />
-  <link href="/booking-sys/src/assets/css/vendor/global.css" rel="stylesheet" />
-
-  <script src="/booking-sys/src/assets/js/vendor/sweet-alert2.js"></script>
-  <script src="/booking-sys/src/assets/css/vendor/toastr/toastr.min.js"></script>
-  <script src="/booking-sys/src/assets/js/vendor/apex-chart.js"></script>
-  <script src="/booking-sys/src/assets/js/vendor/flowbite.min.js"></script>
-</head>
-
-  <header class="fixed w-full z-20 top-0 start-0">
-    <nav class="bg-white border-gray-200 border-b px-4 lg:px-6 py-2.5 dark:bg-gray-800">
-      <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-        <a href="#" class="flex items-center">
-          <img src="/booking-sys/src/assets/img/logo.gif" class="mr-3 h-8 sm:h-9" alt="Logo" />
-          <div>
-            <span class="self-center text-xl text-orange-500 font-semibold whitespace-nowrap">StaySmart</span>
-            <p class="text-xs text-green-600 font-semibold leading-none">Tacloban City</p>
-          </div>
-        </a>
-        <div class="flex items-center lg:order-2">
-          <a href="#" class="text-white bg-orange-600 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2">Log in</a>
-          <a href="#" class="text-white bg-orange-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">Get started</a>
-        </div>
-        <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
-          <ul class="flex flex-col mt-4 gap-2 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-            <li>
-              <a href="#"
-                class="block py-2 text-gray-700 border border-gray-100 hover:bg-orange-500 lg:hover:bg-transparent lg:border-0">Booking</a>
-            </li>
-            <li>
-              <a href="#"
-                class="block py-2 text-gray-700 border border-gray-100 hover:bg-orange-500 lg:hover:bg-transparent lg:border-0">Services</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  </header>
+<?php require_once __DIR__ . '/components/header.php'; ?>
 
 <section class="py-8 bg-white md:py-16 dark:bg-gray-900 antialiased mt-16">
-    <div class="max-w-screen-xl px-4 mx-auto 2xl:px-0">
-      <div class="lg:grid lg:grid-cols-2 lg:gap-8 xl:gap-16">
-        <div class="shrink-0 max-w-md lg:max-w-lg mx-auto">
-          <img class="w-full" src="/booking-sys/src/repositories/uploads/<?= $roomDetails['filename']; ?>" alt="" />
-        </div>
+  <div class="max-w-screen-xl px-4 mx-auto 2xl:px-0">
+    <div class="lg:grid lg:grid-cols-2 lg:gap-8 xl:gap-16">
+      <div class="shrink-0 max-w-lg lg:max-w-lg mx-auto">
+        <img class="w-full rounded-lg" src="/booking-sys/src/repositories/uploads/<?= $roomDetails['filename']; ?>" alt="" />
 
-        <div class="mt-6 sm:mt-8 lg:mt-0">
-          <h1
-            class="text-xl font-semibold text-gray-900 sm:text-2xl dark:text-white"
-          >
-            <?= $roomDetails['title']; ?>
-          </h1>
-          <div class="mt-4 sm:items-center sm:gap-4 sm:flex">
-            <p
-              class="text-2xl font-extrabold text-gray-900 sm:text-3xl dark:text-white"
-            >
-              $1,249.99
-            </p>
+        <div class="grid grid-cols-3 gap-4 pt-4 sm:mt-8 lg:mt-0">
+          <div><img class="w-full rounded-lg" src="/booking-sys/src/repositories/uploads/<?= $roomDetails['filename']; ?>" alt="" /></div>
+          <div><img class="w-full rounded-lg" src="/booking-sys/src/repositories/uploads/<?= $roomDetails['filename']; ?>" alt="" /></div>
+          <div><img class="w-full rounded-lg" src="/booking-sys/src/repositories/uploads/<?= $roomDetails['filename']; ?>" alt="" /></div>
+        </div>
+      </div>
+
+      <div class="mt-6 sm:mt-8 lg:mt-0">
+        <h1 class="text-xl font-semibold text-gray-900 sm:text-2xl dark:text-white">
+          <?= $roomDetails['title']; ?>
+        </h1>
+        <div class="mt-4 sm:items-center sm:gap-4 sm:flex">
+          <p class="text-2xl font-extrabold text-gray-900 sm:text-3xl dark:text-white">
+            &#8369;<?= $roomDetails['price_per_night']; ?>
+            <span class="text-sm text-gray-500 font-normal">(Per Night)</span>
+          </p>
 
             <div class="flex items-center gap-2 mt-2 sm:mt-0">
               <div class="flex items-center gap-1">
@@ -161,58 +115,23 @@ $roomDetails = $roomController->getHotelById($roomId);
           </div>
 
           <div class="mt-6 sm:gap-4 sm:items-center sm:flex sm:mt-8">
-            <a
-              href="#"
-              title=""
-              class="flex items-center justify-center py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-              role="button"
-            >
-              <svg
-                class="w-5 h-5 -ms-2 me-2"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M12.01 6.001C6.5 1 1 8 5.782 13.001L12.011 20l6.23-7C23 8 17.5 1 12.01 6.002Z"
-                />
-              </svg>
+            <a href="#"
+              class="flex items-center justify-center py-2.5 px-5 text-sm font-medium text-gray-900 hover:bg-orange-500 hover:text-gray-100 bg-white rounded-lg border border-gray-200"
+              role="button">
+              <i class="fa-regular fa-heart pr-1"></i>
               Add to favorites
             </a>
 
-            <a
-              href="#"
-              title=""
-              class="text-white mt-4 sm:mt-0 bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 flex items-center justify-center"
-              role="button"
-            >
-              <svg
-                class="w-5 h-5 -ms-2 me-2"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M4 4h1.5L8 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm.75-3H7.5M11 7H6.312M17 4v6m-3-3h6"
-                />
-              </svg>
+            <button 
+              data-modal-target="rate-modal" 
+              data-modal-toggle="rate-modal" 
+              class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center" 
+              type="button">
+              <i class="fa-regular fa-star"></i>
+              Leave a Review
+            </button>
 
-              Add to cart
-            </a>
+            <?php require_once 'modals/rate-modal.php';?>
           </div>
 
           <hr class="my-6 md:my-8 border-gray-200 dark:border-gray-800" />
@@ -230,3 +149,5 @@ $roomDetails = $roomController->getHotelById($roomId);
       </div>
     </div>
   </section>
+
+    <?php require_once __DIR__ . '/components/footer.php'; ?>
