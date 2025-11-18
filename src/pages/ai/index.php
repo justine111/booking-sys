9d0242ae5ee4ae287278e142e1989d9dd5069760
justine111 <90599321+justine111@users.noreply.github.com>
2025-11-18 15:33:14 +0800

    commit

--- src/pages/ai/index.php
+++ src/pages/ai/index.php
@@ -39,11 +39,7 @@
             class="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50"
             placeholder="Ask Smart">
           <button onclick="sendMessage()" class="ml-2 px-4 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
-<<<<<<< HEAD
             Send
-=======
-            <i data-lucide="send-horizontal" class="w-[20px]"></i>
->>>>>>> 8ed66ffd097f8f0c660d5d7e0bab9bb3a5b8d9c4
           </button>
         </div>
       </div>
@@ -72,6 +68,6 @@
     }
   </script> -->
 
-  <script>
-    lucide.createIcons();
-  </script>
+<script>
+  lucide.createIcons();
+</script>
